8e3a05cd65ccf8db5369a25cc6e5db1c2e790ab5
Jennifer <Jennifer@DESKTOP-R39PCAT>
2017-03-28 10:18:47 -0300

    Pantalla ListarCampaña

--- CarteleriaDigital/CarteleriaDigital/Pantallas/PanCampaña.Designer.cs
+++ CarteleriaDigital/CarteleriaDigital/Pantallas/PanCampaña.Designer.cs
@@ -32,9 +32,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PanCampaña));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
@@ -62,46 +60,20 @@
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
-            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.button3.Location = new System.Drawing.Point(175, 85);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(102, 47);
-            this.button3.TabIndex = 2;
-            this.toolTip1.SetToolTip(this.button3, "Eliminar Campaña");
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // button4
             // 
             this.button4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
             this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.button4.Location = new System.Drawing.Point(310, 85);
+            this.button4.Location = new System.Drawing.Point(185, 85);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(102, 47);
             this.button4.TabIndex = 3;
-            this.toolTip1.SetToolTip(this.button4, "Modificar Campaña");
+            this.toolTip1.SetToolTip(this.button4, "Listar Campaña");
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button5
-            // 
-            this.button5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
-            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.button5.Location = new System.Drawing.Point(445, 85);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(102, 47);
-            this.button5.TabIndex = 4;
-            this.toolTip1.SetToolTip(this.button5, "Vista Previa Campaña");
-            this.button5.UseVisualStyleBackColor = false;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // Campaña
+            // PanCampaña
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -109,13 +81,11 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(634, 561);
-            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Campaña";
+            this.Name = "PanCampaña";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Campaña";
             this.Load += new System.EventHandler(this.Campaña_Load);
@@ -127,9 +97,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.ToolTip toolTip1;
-        private System.Windows.Forms.Button button5;
     }
 }
