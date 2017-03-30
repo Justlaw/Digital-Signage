e772b4ccb46b0208b6d1620aae5e2a3d05353bee
JS73 <salinasjona73@gmail.com>
2017-03-30 11:02:20 -0300

    Clase estática "Connection"

---
+++ CarteleriaDigital/CarteleriaDigital/DAO/Postgres/Connection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace CarteleriaDigital.DAO
+{
+    static class Connection
+    {
+        static string stringConexion = "Server= locarhost; Port= 5432; User Id= postgres; Password= 123; Database= DBCarteleria;";
+        public static NpgsqlConnection con = new NpgsqlConnection(stringConexion);
+    }
+}
