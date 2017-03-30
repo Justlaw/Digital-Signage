3bc4745094aaccc4a91bf3b67f5df2bb5187478e
Jennifer <Jennifer@DESKTOP-R39PCAT>
2017-03-30 11:32:00 -0300

    Btn filtrar

--- CarteleriaDigital/CarteleriaDigital/Pantallas/ListarCampaña.cs
+++ CarteleriaDigital/CarteleriaDigital/Pantallas/ListarCampaña.cs
@@ -7,13 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CarteleriaDigital.DTO;
+using CarteleriaDigital.DAO;
 
 namespace CarteleriaDigital.Pantallas
 {
-    public partial class ListarCampaña : Form
+    public partial class ListarCampaña : Form        
     {
+        CampañaDAO camp = new CampañaDAO();
+
         public ListarCampaña()
         {
+            List<CampañaDTO> Campañas = new List<CampañaDTO>();
             InitializeComponent();
         }
 
@@ -25,6 +30,20 @@ namespace CarteleriaDigital.Pantallas
 
             PanCampaña abrir = new PanCampaña ();
             abrir.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Segun el tipo de busqueda seleccionada en el ComboBox, se realizara la operacion que corresponda.
+            
+                    {   //Carga el datasource con las campañas cuyas fechas se encuentren en el intervalo.
+                        List<CampañaDTO> campañasFecha = new List<CampañaDTO>();
+                campañasFecha = 
+                        
+
+                        dataGridView1.DataSource = campañasFecha;
+                 
+            
         }
     }
 }
