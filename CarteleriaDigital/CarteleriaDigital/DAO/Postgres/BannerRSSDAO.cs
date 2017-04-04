c503c9def222545b23f6b82236c5dca46302d5f6
Dan <Dan@DAN-PC>
2017-04-04 09:36:38 -0300

    Insertar y Modificar BannerRSS

--- CarteleriaDigital/CarteleriaDigital/DAO/Postgres/BannerRSSDAO.cs
+++ CarteleriaDigital/CarteleriaDigital/DAO/Postgres/BannerRSSDAO.cs
@@ -4,12 +4,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Npgsql;
+using CarteleriaDigital.DTO;
 
 
 namespace CarteleriaDigital.DAO
 {
     class BannerRSSDAO
     {
-        
+        public BannerRSSDAO()
+        {
+
+        }
+
+        public void Insertar(BannerRSSDTO bRSSDTO)
+        {
+            try
+            {
+                Connection.con.Open();
+                // Create insert command.
+                NpgsqlCommand command = new NpgsqlCommand("INSERT INTO " +
+                    "bannerrss(idbanner, fuenterss) VALUES(:idbanner, :fuenterss)", Connection.con);
+
+                command.Parameters.AddWithValue("@idrango", bRSSDTO.IdBanner);
+                command.Parameters.AddWithValue("@fuenterss", bRSSDTO.FuenteRSS);
+
+                // Execute SQL command.
+                Int32 recordAffected = command.ExecuteNonQuery();
+                if (Convert.ToBoolean(recordAffected))
+                {
+                    //Mostrar
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                //Mostrar error
+            }
+
+            Connection.con.Close();
+        }
+
+        public void Modificar(BannerRSSDTO bRSSDTO)
+        {
+            Connection.con.Open();
+
+            try
+            {
+                // Create update command.
+                NpgsqlCommand command = new NpgsqlCommand(@"UPDATE Banner " +
+                    "SET idbanner = @idbanner, fuenterss = @fuenterss WHERE idbannerRSS = " + bRSSDTO.IdBannerRSS, Connection.con);
+
+                // Add paramaters.
+                command.Parameters.AddWithValue("@idbanner", bRSSDTO.IdBanner);
+                command.Parameters.AddWithValue("@fuenterss", bRSSDTO.FuenteRSS);
+
+
+                // Execute SQL command.
+                int recordAffected = command.ExecuteNonQuery();
+                if (Convert.ToBoolean(recordAffected))
+                {
+                    //showInformation("Data successfully updated!");
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                //showError(ex);
+            }
+
+            Connection.con.Close();
+        }
     }
 }
