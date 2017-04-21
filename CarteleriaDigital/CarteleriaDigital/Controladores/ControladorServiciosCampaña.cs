186571284c12cb7c8f8d95b6c8fe8dfdf287eadb
Daniel Adrián Grinman <dan.gr04@live.com>
2017-04-21 10:42:09 -0300

    Modificar campaña + Eliminar Imagen DAO

--- CarteleriaDigital/CarteleriaDigital/Controladores/ControladorServiciosCampaña.cs
+++ CarteleriaDigital/CarteleriaDigital/Controladores/ControladorServiciosCampaña.cs
@@ -24,11 +24,12 @@ namespace CarteleriaDigital.Controladores
 
 
 
-            foreach (Imagen img in listImg) {
+            foreach (Imagen img in listImg)
+            {
                 ImagenDTO img_DTO = new ImagenDTO();
                 img_DTO.Duracion = img.Duracion;
                 img_DTO.RutaImagen = img.RutaImagen;
-                listImgDTO.Add(img_DTO); 
+                listImgDTO.Add(img_DTO);
             }
 
             RangoDTO rng_DTO = new RangoDTO();
@@ -36,7 +37,7 @@ namespace CarteleriaDigital.Controladores
             rng_DTO.FechaFin = rng.FechaFin;
             rng_DTO.HoraInicio = rng.HoraInicio;
             rng_DTO.HoraFin = rng.HoraFin;
-            
+
             //Se controla que el rango esté disponible y en caso que lo esté, se procede a insertar la campaña.
             if (rng_DAO.RangoDisponible(rng_DTO))
             {
@@ -49,11 +50,12 @@ namespace CarteleriaDigital.Controladores
                 {
                     imgDTO.IdCampaña = camp_DAO.ObtenerUltimoId();
                     Img_DAO.Insertar(imgDTO);
-                }                  
+                }
             }
         }
 
-        public void Baja(String nombreCampaña) {
+        public void Baja(String nombreCampaña)
+        {
             CampañaDAO camp_DAO = new CampañaDAO();
             CampañaDTO camp_DTO = new CampañaDTO();
 
@@ -64,12 +66,59 @@ namespace CarteleriaDigital.Controladores
             camp_DAO.Modificar(camp_DTO);
         }
 
-        public void Modificar(Campaña camp) {
+        public void Modificar(CampañaDTO camp_DTO, RangoDTO rng_DTO, List<ImagenDTO> listImg_DTO, List<ImagenDTO> listImgV_DTO)
+        {
             CampañaDAO camp_DAO = new CampañaDAO();
             ImagenDAO Img_DAO = new ImagenDAO();
             RangoDAO rng_DAO = new RangoDAO();
 
-            
+            if (rng_DTO != null)
+            {
+                if (rng_DAO.RangoDisponible(rng_DTO))
+                {
+                    rng_DAO.Modificar(rng_DTO);
+                }
+            }
+            if (camp_DTO != null)
+            {
+                camp_DAO.Modificar(camp_DTO);
+            }
+            if (listImg_DTO != null)
+            {
+                Boolean encontrada;
+                foreach (ImagenDTO img_DTO in listImg_DTO)
+                {
+                    encontrada = false;
+                    foreach (ImagenDTO img_DTO2 in listImgV_DTO)
+                    {
+                        if (img_DTO.IdImagen == img_DTO2.IdImagen)
+                        {
+                            encontrada = true;
+                            break;
+                        }
+                    }
+                    if (encontrada == false)
+                    {
+                        Img_DAO.Insertar(img_DTO);
+                    }
+                }
+                foreach (ImagenDTO img in listImgV_DTO)
+                {
+                    encontrada = false;
+                    foreach (ImagenDTO img2 in listImg_DTO)
+                    {
+                        if (img.IdImagen == img2.IdImagen)
+                        {
+                            encontrada = true;
+                            break;
+                        }
+                    }
+                    if (encontrada == false)
+                    {
+                        Img_DAO.Eliminar(img.IdImagen.Value);
+                    }
+                }
+            }            
         }
     }
 }
